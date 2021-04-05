a1dc7a5fdc866c7726bd9ca831ff496c745d34ef
Sýlvan Heuser <sylvan.heuser@gmx.net>
2021-04-05 08:08:31 +0200

    Add debug logging

--- dewpoint.py
+++ dewpoint.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python3
 
 import argparse
+import logging
 import math
+
+log = logging.getLogger(__name__)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+
+
+if __name__ == '__main__':
+    main()
 
 # Magnus parameters between -45°C and 60°C
 alpha = 6.112  # hPA
@@ -10,18 +21,25 @@ gamma = 243.12  # °C
 
 
 def saturation_vapour_pressure(temperature: float) -> float:
-    return alpha * math.pow(
+    svp = alpha * math.pow(
         math.e, (beta * temperature) / (gamma + temperature)
     )
+    log.debug(f'saturation vapour pressure at {temperature}°C: {svp}hPA')
+    return svp
 
 
 def vapour_pressure(temperature: float, relative_humidity: float) -> float:
-    return relative_humidity * saturation_vapour_pressure(temperature) / 100
+    vp = relative_humidity * saturation_vapour_pressure(temperature) / 100
+    log.debug(
+        f'vapour pressure at {temperature}°C and {relative_humidity}%RH: {vp}hPA'
+    )
+    return vp
 
 
 def dewpoint(temperature: float, relative_humidity: float) -> float:
     vp_over_alpha = vapour_pressure(temperature, relative_humidity) / alpha
     dp1 = (gamma * math.log(vp_over_alpha)) / (beta - math.log(vp_over_alpha))
+    log.debug(f'{dp1=}°C')
     dp_first_part = gamma * (
         math.log(relative_humidity / 100) + ((beta * temperature) /
                                              (gamma + temperature))
@@ -44,7 +62,15 @@ def main():
     parser.add_argument(
         'relative_humidity', type=float, help='relative humidity in percent'
     )
+    parser.add_argument(
+        '--loglevel', default='WARNING', help="Loglevel", action='store'
+    )
     args = parser.parse_args()
+    loglevel = getattr(logging, args.loglevel.upper(), None)
+    if not isinstance(loglevel, int):
+        raise ValueError('Invalid log level: {}'.format(args.loglevel))
+    logging.basicConfig(level=loglevel)
+
     t = args.temperature
     rh = args.relative_humidity
     dp = dewpoint(t, rh)
