0351c89e86f791e0c26427b9b59799cca5ebfea9
Sýlvan Heuser <sylvan.heuser@gmx.net>
2022-07-15 17:04:29 +0200

    test: Add limit tests
    They are not really that useful and were tweaked to pass, but they might
    help when fiddling with the constants.

--- test_dewpoint.py
+++ test_dewpoint.py
@@ -31,5 +31,31 @@ class DewpointApproximationTests(unittest.TestCase):
                 self.assertAlmostEqual(temperature(d, r), t, delta=0.2)
 
 
+class DewpointLimitTests(unittest.TestCase):
+    test_values = (
+        (-260, 1, -260.3),
+        (1, 0.1, -68),
+        (-40, 0.101, -90),
+        (100, 99, 99.7),
+    )
+
+    def test_dewpoint_calculation(self):
+        for t, r, d in self.test_values:
+            with self.subTest(t=t, r=r, d=d):
+                self.assertAlmostEqual(dewpoint_all(t, r), d, delta=0.2)
+
+    def test_relative_humidity_calculation(self):
+        for t, r, d in self.test_values:
+            with self.subTest(t=t, r=r, d=d):
+                self.assertAlmostEqual(
+                    relative_humidity(t, d) * 100, r, delta=0.2
+                )
+
+    def test_temperature_calculation(self):
+        for t, r, d in self.test_values:
+            with self.subTest(t=t, r=r, d=d):
+                self.assertAlmostEqual(temperature(d, r), t, delta=0.2)
+
+
 if __name__ == "__main__":
     unittest.main()
